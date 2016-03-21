b71bf58e253b6818b879ff0a446d2a2a2a3427ee
August Valera <4U6U57@gmail.com>
2016-03-20 19:50:57 -0700

    Refactored plugins to plugin.mk

--- Makefile
+++ Makefile
@@ -1,86 +1,21 @@
 SOURCE=vimrc
 OUTPUT=.vimrc
-DEPEND=pathogen auto-pairs html5.vim undotree supertab vim-airline vim-autoformat vim-colors-solarized vim-gitgutter vim-json #$(OPTION)
-# Uncomment above to enable optional plugins
-OPTION=nerdtree nerdtree-git-plugin
-VIMBIN=~/.vim
-VIMBUN=$(VIMBIN)/bundle
 
 default : pull
 	make install
 
-install : $(DEPEND) $(OUTPUT)
+install : $(OUTPUT) pluginstall
 	mv -f $(OUTPUT) ~
 
 $(OUTPUT) : $(SOURCE)
 	cp $(SOURCE) $@
 
-pathogen :
-	mkdir -p $(VIMBIN)/autoload $(VIMBUN)
-	curl -LSso $(VIMBIN)/autoload/$@.vim https://tpo.pe/$@.vim
-
-# Auto add matching brackets
-auto-pairs :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/jiangmiao/$@.git $(VIMBUN)/$@
-
-# HTML5 smart indentation
-html5.vim :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/othree/$@.git $(VIMBUN)/$@
-
-# Adds directory browsing toolbar (change .vimrc as well)
-nerdtree :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/scrooloose/$@.git $(VIMBUN)/$@
-
-nerdtree-git-plugin :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/Xuyuanp/$@.git $(VIMBUN)/$@
-
-# Autocomplete with tab key
-supertab :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/ervandew/$@.git $(VIMBUN)/$@
-
-# Adds undo sidebar
-undotree :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/mbbill/$@.git $(VIMBUN)/$@
-
-# Adds fancy bottom bar
-vim-airline :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/bling/$@.git $(VIMBUN)/$@
-
-# Adds official autoformat
-vim-autoformat :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/Chiel92/$@.git $(VIMBUN)/$@
-
-# Adds cool colortheme
-vim-colors-solarized :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/altercation/$@.git $(VIMBUN)/$@
-
-# Adds git sidebar +/-
-vim-gitgutter :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/airblade/$@.git $(VIMBUN)/$@
-
-# JSON smart indentation
-vim-json :
-	rm -rf $(VIMBUN)/$@
-	git clone https://github.com/elzr/$@.git $(VIMBUN)/$@
-
 clean :
-	rm -rf $(OUTPUT)
+	rm -f $(OUTPUT)
 
-spotless : clean 
+spotless : clean plugclear
 
-uninstall : spotless
-	rm -rf ~/$(OUTPUT) $(VIMBIN)
-
-.PHONY : default install clean spotless $(DEPEND) $(OPTION)
+.PHONY : default install clean spotless
 
 include git.mk
+include plugins.mk
---
+++ plugins.mk
@@ -0,0 +1,98 @@
+# plugins.mk
+# Plugins for vim
+
+ENABLED=pathogen # important, needed for all plugins
+
+# OPTIONAL PLUGINS BELOW
+# Uncomment those you want enabled
+ENABLED+= auto-pairs # Auto add matching brackets
+ENABLED+= html5.vim # HTML5 support
+# ENABLED=$(ENABLED) nerdtree nerdtree-git-plugin # Directory browsing toolbar
+ENABLED+= supertab # Autocomplete with tab key
+ENABLED+= undotree # Undo sidebar
+ENABLED+= vim-airline # Fancy bottom bar
+ENABLED+= vim-autoformat # Official autoformat
+ENABLED+= vim-colors-solarized # Cool colortheme
+ENABLED+= vim-gitgutter # Git +/-
+ENABLED+= vim-json # JSON support
+
+VIMBIN=~/.vim
+VIMBUN=$(VIMBIN)/bundle
+
+default : plugugrade
+
+# Installs newly enabled plugins
+plugupgrade : $(ENABLED)
+
+# Same as above, but also removes all other plugins
+pluginstall : plugclear plugupgrade
+
+# Removes all plugins
+plugclear :
+	rm -rf $(VIMBUN)
+
+.PHONY : default plugugrade pluginstall plugclear
+
+# Plugin download information below
+
+pathogen :
+	mkdir -p $(VIMBIN)/autoload $(VIMBUN)
+	curl -LSso $(VIMBIN)/autoload/$@.vim https://tpo.pe/$@.vim
+
+auto-pairs :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/jiangmiao/$@.git $(VIMBUN)/$@
+
+html5.vim :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/othree/$@.git $(VIMBUN)/$@
+
+nerdtree :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/scrooloose/$@.git $(VIMBUN)/$@
+
+nerdtree-git-plugin :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/Xuyuanp/$@.git $(VIMBUN)/$@
+
+supertab :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/ervandew/$@.git $(VIMBUN)/$@
+
+undotree :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/mbbill/$@.git $(VIMBUN)/$@
+
+vim-airline :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/bling/$@.git $(VIMBUN)/$@
+
+vim-autoformat :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/Chiel92/$@.git $(VIMBUN)/$@
+
+vim-colors-solarized :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/altercation/$@.git $(VIMBUN)/$@
+
+vim-gitgutter :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/airblade/$@.git $(VIMBUN)/$@
+
+vim-json :
+	rm -rf $(VIMBUN)/$@
+	git clone https://github.com/elzr/$@.git $(VIMBUN)/$@
+
+PLUGINS=""
+PLUGINS+= auto-pairs
+PLUGINS+= html5.vim
+PLUGINS+= nerdtree nerdtree-git-plugin
+PLUGINS+= supertab
+PLUGINS+= undotree
+PLUGINS+= vim-airline
+PLUGINS+= vim-autoformat
+PLUGINS+= vim-colors-solarized
+PLUGINS+= vim-gitgutter
+PLUGINS+= vim-json
+
+.PHONY : $(PLUGINS)
